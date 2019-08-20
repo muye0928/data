570bc8228550442a46433d674ab12786fb26a372
Iris <muye0928@gmail.com>
2019-08-20 19:08:08 +0800

    Update coach小程序留存.sql

--- coach小程序留存.sql
+++ coach小程序留存.sql
@@ -2,7 +2,7 @@
 
 set hive.exec.dynamic.partition=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
-insert overwrite table keep_app.rpt_kl_tmp1 partition (month_last_day)
+insert overwrite table keep_temp.rpt_kl_mini_all partition (month_last_day)
 
   select current1.schedulenum as class_count,
        users.usernum as users_count,
@@ -80,3 +80,81 @@ left outer join
    group by lastmonth.monthly,lastmonth.coachuserid
    )retention
 on current1.coachuserid = retention.coachuserid and current1.monthly = retention.monthly
+
+---增量跑
+
+set hive.exec.dynamic.partition=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+insert overwrite table keep_temp.rpt_kl_mini_all partition (month_last_day = "2019-08-31")
+  select current1.schedulenum as class_count,
+       users.usernum as users_count,
+       current1.coachuserid,
+       retention.retain_cnt as retention_count,
+       retention.retain_rate as retention_rate
+from
+  (select count(distinct course.schedule_id) as schedulenum,
+        coachuserid
+  from
+    (select schedule_id
+    from keep_dw.dwd_kl_schedule_new
+         where schedule_status = 2 and date >="2019-08-01" and date <="2019-08-31"
+    )course
+  left outer join
+   (select scheduleid,coachuserid
+    from keep_ods.ods_kl_schedule_coach_relation
+    where p_date = "2019-08-19"
+    )relation
+  on course.schedule_id = relation.scheduleid
+  group by coachuserid
+  )current1
+  left outer join
+  (select count(distinct user_id)as usernum,coach_user_id
+    from keep_dw.dwd_kl_order_new
+    where status in (2,3,4,5,6) and date >="2019-08-01" and date <="2019-08-31"
+    group by coach_user_id 
+    )users 
+  on users.coach_user_id = current1.coachuserid 
+left outer join
+  (select 
+     count(lastmonth.user_id) as last_cnt,
+     lastmonth.coachuserid,
+     count(now.user_id) as retain_cnt,
+     if (count(lastmonth.user_id) = 0,null,count(now.user_id)/count(lastmonth.user_id) )as retain_rate
+     from      
+    (select user_id,
+            coachuserid      
+    from
+         (select user_id,last_day(date) as monthly,schedule_id
+          from keep_dw.dwd_kl_order_new
+          where status in(2,3,4,5,6) and date >="2019-07-01" and date <="2019-07-31"
+          )last
+          left outer join
+         (select scheduleid,
+                 coachuserid
+         from keep_ods.ods_kl_schedule_coach_relation
+         where p_date = "2019-08-19"
+         )relation1
+         on last.schedule_id = relation1.scheduleid
+         group by user_id,coachuserid
+          )lastmonth
+     left outer join
+     (select user_id,
+             coachuserid
+             
+   from
+  (select user_id,schedule_id
+  from keep_dw.dwd_kl_order_new
+  where status in(2,3,4,5,6) and date >="2019-08-01" and date <="2019-08-31")recent
+  left outer join
+  (select scheduleid,coachuserid
+   from keep_ods.ods_kl_schedule_coach_relation
+   where p_date = "2019-08-19")relation1
+  on recent.schedule_id = relation1.scheduleid
+  group by user_id,coachuserid
+   )now
+   on now.user_id = lastmonth.user_id and 
+      now.coachuserid = lastmonth.coachuserid
+   group by lastmonth.coachuserid
+   )retention
+on current1.coachuserid = retention.coachuserid 
+
