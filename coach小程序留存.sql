9390900b9cc2bdc1b85c9e9aa495d01ab25b7ed7
Iris <muye0928@gmail.com>
2019-08-23 17:17:54 +0800

    Update coach小程序留存.sql

--- coach小程序留存.sql
+++ coach小程序留存.sql
@@ -158,3 +158,96 @@ left outer join
    )retention
 on current1.coachuserid = retention.coachuserid 
 
+
+#coach复购分课程
+create table if not exists keep_temp.rpt_kl_mini_courses
+( coachuserid string,
+  base_id string,
+  retention_count int comment "本月该教练该课程留存用户",
+  users_count int comment "本月该教练该课程用户",
+  course_count int comment "本月该教练该课程授课节数"
+ )
+ comment "keepland教练小程序分课程留存"
+  partitioned by (monthly string)
+  STORED AS PARQUET;
+
+  #sql total
+  insert overwrite table keep_temp.rpt_kl_mini_courses partition(monthly)
+  select schedule_info.coachuserid,
+         schedule_info.base_id,
+         retention.retention_users,
+         schedule_info.usernum,
+         schedule_info.course_count,
+         substring(schedule_info.monthly,0,7) as monthly
+         from
+  (select count(distinct user_id) as usernum,
+          count(distinct schedule_id) as course_count,
+          base_id,
+          coachuserid,
+          monthly
+  from
+  (select schedule_id,
+          base_id,
+          user_id,
+          last_day(date) as monthly 
+  from keep_dw.dwd_kl_order_new
+  where status in (2,3,4,5,6))book_info 
+  left outer join
+  (select coachuserid,
+          scheduleid
+    from keep_ods.ods_kl_schedule_coach_relation
+    where p_date = "2019-08-22")relation 
+  on book_info.schedule_id = relation.scheduleid
+  group by base_id,coachuserid,last_day(date)
+  )schedule_info
+
+left outer join
+
+
+(select 
+        count(course_level.user_id) as retention_users,
+        course_level.monthly,
+        course_level.base_id,
+        course_level.coachuserid
+
+from 
+(select user_id,
+        schedule_id,
+        last_day(date) as monthly
+        from keep_dw.dwd_kl_order_new
+        where status in (2,3,4,5,6)
+        )last1
+        left outer join
+        (select scheduleid,coachuserid
+          from keep_ods.ods_kl_schedule_coach_relation
+          where p_date = "2019-08-22")relation1
+        on last1.schedule_id = relation1.scheduleid
+        group by last_day(date),user_id
+        )last2
+inner join
+（select user_id,
+        monthly,
+        coachuserid,
+        base_id 
+from
+        (select user_id,
+        schedule_id,
+        last_day(date) as monthly,
+        base_id
+        from keep_dw.dwd_kl_order_new
+        where status in (2,3,4,5,6)
+        )now
+        left outer join
+        (select scheduleid,coachuserid
+          from keep_ods.ods_kl_schedule_coach_relation
+          where p_date = "2019-08-22")relation2
+        on now.schedule_id = relation2.scheduleid
+        group by last_day(date), user_id,coachuserid,base_id
+        ）course_level
+  on last2.user_id = course_level.user_id and last2.monthly = add_months(course_level.monthly,-1) and last2.coachuserid = course_level.coachuserid
+)retention 
+
+on schedule_info.coachuserid = retention.coachuserid
+and schedule_info.base_id = retention.base_id
+and schedule_info.monthly = retention.monthly
+
